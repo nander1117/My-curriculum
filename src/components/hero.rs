512c9b81facc621c21faae64b656cdff9d540abd
nander1117 <jhonander1117@gmail.com>
2025-07-31 22:14:16 -0400

    test(dioxus-free-icons): testing dioxus free icons

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -11,15 +11,20 @@ pub fn Hero() -> Element {
     rsx! {
         div { id: "hero", class: "bg-gray-900",
             img { src: HEADER_SVG, id: "header" }
-            label { class: "has-invalid:text-blue-700 shadow  shadow-white p-2 m-5 bg-white border-2 border-amber-300 rounded text-black text-center flex items-center justify-center",
+            label { class: "peer has-invalid:text-blue-700 shadow  shadow-white p-2 m-5 bg-white border-2 border-amber-300 rounded text-black text-center flex items-center justify-center",
                 input {
                     required: true,
                     pattern: "[a-zA-Z0-9_]+",
                     oninput: move |e| increment(e, &mut count),
-                    class: "text-black text-center invalid:text-red-500 outline-none select-none",
+                    class: " text-black text-center invalid:text-red-500 outline-none select-none",
                     placeholder: "Type something here...",
                 }
                 h1 { class: "p-2", "{count}" }
+            }
+            button {
+                class: " peer-has-invalid:bg-blue-800  bg-amber-600 p-2 rounded-2xl border border-amber-300  ",
+                onclick: move |_| count.set(0),
+                "👋 Hello Dioxus"
             }
             div { id: "links",
                 // The RSX macro also supports text nodes surrounded by quotes
