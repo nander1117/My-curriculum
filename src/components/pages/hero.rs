414bf8bc33eff147f1f6ed145f236afac0b7fc67
nander1117 <jhonander1117@gmail.com>
2025-08-11 23:46:34 -0400

    testing

--- src/components/pages/hero.rs
+++ src/components/pages/hero.rs
@@ -8,12 +8,12 @@ pub fn Hero() -> Element {
     fn increment(e: Event<FormData>, count: &mut Signal<usize>) {
         count.set(e.data().value().len());
     }
-    rsx! {
+    rsx! (
         div { id: "hero", class: "bg-gray-900",
             img { src: HEADER_SVG, id: "header" }
             label { class: "peer has-invalid:text-blue-700 shadow  shadow-white p-2 m-5 bg-white border-2 border-amber-300 rounded text-black text-center flex items-center justify-center",
                 input {
-                    required: false,
+                    required: true,
                     pattern: "[a-zA-Z0-9_]+",
                     oninput: move |e| increment(e, &mut count),
                     class: " text-black text-center invalid:text-red-500 outline-none select-none",
@@ -38,5 +38,5 @@ pub fn Hero() -> Element {
                 a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
             }
         }
-    }
+    )
 }
