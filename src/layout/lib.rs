f131ae4f8c21d2cca473050dd97d6f62e654aadd
nander1117 <jhonander1117@gmail.com>
2025-10-05 20:33:58 -0400

    feat(layout\lib): add block and add dock bar mobile

--- src/layout/lib.rs
+++ src/layout/lib.rs
@@ -1,4 +1,4 @@
-use crate::layout::{Navbar, Sidebar, SidebarMobile};
+use crate::layout::{DockBarMobile, Navbar, Sidebar, SidebarMobile};
 use crate::routes::Route;
 use dioxus::prelude::*;
 
@@ -7,11 +7,26 @@ pub fn Layout() -> Element {
     rsx! (
         SidebarMobile {}
         Navbar {}
-        div { class: "flex overflow-hidden max-h-[calc(100dvh-60px)] ",
+        div { class: "flex overflow-hidden md:max-h-[calc(100dvh-72.60px)] sm:max-h-[calc(100dvh-61px-4rem)] max-h-[calc(100dvh-56px-4rem)] h-full",
             Sidebar {}
-            main { class: " p-4 overflow-y-auto bg-gray-100 dark:bg-gray-800 w-full motion animation-section",
-                div { class: "w-full overflow-hidden shadow rounded-2xl", Outlet::<Route> {} }
+            main { class: " flex overflow-y-auto bg-white dark:bg-gray-950 w-full motion animation-section ",
+                div { class: "flex w-full h-fit min-h-full",
+                    Block {}
+                    Outlet::<Route> {}
+                    Block {}
+                }
             }
+        }
+        DockBarMobile {}
+    )
+}
+
+#[component]
+fn Block() -> Element {
+    rsx!(
+        div {
+            id: "Blok",
+            class: " w-7 grow border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-gray-200)] md:block dark:[--pattern-fg:var(--color-gray-700)] md:min-h-[calc(100dvh-72.60px)] sm:min-h-[calc(100dvh-61px-4rem)] min-h-[calc(100dvh-56px-4rem)]",
         }
     )
 }
