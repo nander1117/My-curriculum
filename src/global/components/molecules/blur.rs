8817a25c0768ad0f30dc76d05cbad83dd914f818
nander1117 <jhonander1117@gmail.com>
2025-10-10 20:21:57 -0400

    feat(blur): enhance Blur component with z-index for improved layering

--- src/global/components/molecules/blur.rs
+++ src/global/components/molecules/blur.rs
@@ -8,7 +8,11 @@ pub fn Blur() -> Element {
     rsx!(
         div {
             style: format!("background-image : url({});", BLUR_BACKGROUND),
-            class: "size-full fixed bg-[length:30px]! left-0 pointer-events-none top-0  opacity-[0.05] bg-blend-multiply bg-repeat",
+            class: "size-full fixed bg-[length:30px]! left-0 pointer-events-none top-0  opacity-[0.05] bg-blend-multiply bg-repeat z-20",
+        }
+        div { class: "hidden dark:block absolute inset-0 opacity-20 pointer-events-none z-20",
+            div { class: "absolute top-20 left-10 w-40 h-40 rounded-full bg-purple-600 blur-3xl" }
+            div { class: "absolute bottom-20 right-10 w-60 h-60 bg-cyan-600 blur-3xl" }
         }
     )
 }
