d4e604139d57dfc0e1f098a10bf395ec922ff34f
nander1117 <jhonander1117@gmail.com>
2025-10-11 20:11:09 -0400

    add(porfolio): created section for porfolio

--- src/pages/not_found/lib.rs
+++ src/pages/not_found/lib.rs
@@ -5,7 +5,7 @@ use dioxus::prelude::*;
 pub fn NotFound(route: Vec<String>) -> Element {
     rsx! {
 
-        section { class: "relative min-h-screen bg-gray-900 overflow-hidden flex items-center justify-center  p-6",
+        section { class: "relative min-h-screen bg-gray-950 overflow-hidden flex items-center justify-center  p-6",
             div { class: "absolute inset-0 opacity-20",
                 div { class: "absolute top-20 left-10 w-40 h-40 rounded-full bg-purple-600 blur-3xl" }
                 div { class: "absolute bottom-20 right-10 w-60 h-60 bg-cyan-600 blur-3xl" }
@@ -29,7 +29,7 @@ pub fn NotFound(route: Vec<String>) -> Element {
                     " Oops! The page you're looking for has vanished into the digital void. "
                 }
                 Link {
-                    to: Route::Home {},
+                    to: Route::Porfolio {},
                     class: " btn relative px-8 py-4 bg-gradient-to-r from-purple-600 to-cyan-600 text-white font-bold rounded-lg overflow-hidden group",
                     span { class: "relative z-10", "Return to Home" }
                     span { class: "absolute inset-0 bg-gradient-to-r from-cyan-600 to-purple-600 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg" }
